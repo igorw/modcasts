0777339f8af14d03b8d51ea5a1880533ce07e69f
Igor Wiedler <igor@wiedler.ch>
2010-05-02 18:09:59 +0200

    Artist, Episode, License entities

---
+++ app/model/Artist.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of modcasts.
+ *
+ * (c) 2010 Igor Wiedler
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Modcasts;
+
+class Artist {
+	public $name;
+	public $homepage;
+}
---
+++ app/model/Episode.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of modcasts.
+ *
+ * (c) 2010 Igor Wiedler
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Modcasts;
+
+class Episode {
+	public $id;
+	public $title;
+	public $created_at;
+	public $file_bytes;
+	public $theme_artist;
+	public $theme_license;
+}
