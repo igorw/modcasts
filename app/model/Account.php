f3e492c8a341d6ce620334bf6e0e6624e22defbf
Igor Wiedler <igor@wiedler.ch>
2010-05-03 17:44:32 +0200

    add Account model

---
+++ app/model/Account.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of modcasts.
+ *
+ * (c) 2010 Igor Wiedler
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Modcasts\Entities;
+
+/** @Entity */
+class Account {
+	/** @Id @Column(type="integer") @GeneratedValue(strategy="AUTO") */
+	public $id;
+	/** @Column(length=100) */
+	public $username;
+	/** @Column(length=100) */
+	public $realname;
+	/** @Column(length=64) */
+	public $password;
+	/** @Column(type="datetime") */
+	public $created;
+	
+	public function __construct() {
+		$this->created = new \DateTime("now");
+	}
+}
